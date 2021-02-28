e85136662eb0e62878cf483a0ad30cb7bd432292
Reid Johnson <rjsa210@gmail.com>
2021-02-27 22:39:48 -0800

    full functionality achieved -RJ

--- Develop/script.js
+++ Develop/script.js
@@ -7,21 +7,21 @@ var inputFill;
 
 //append a span under the title that shows current day and time
 var dateTimeNow = moment().format('dddd, MMMM Do YYYY, h:mm a');
-var currentHour = moment().format('h a')
+var currentHour = moment('14:00:00', 'hh:mm:ss').format('hh a')
 $('#currentDay').text(dateTimeNow);
 
 //Create and Handle array with index of 7, each i is a seperate hour in work day 9-5
 
 var hours = [
-  '9 am',
-  '10 am',
-  '11 am',
-  '12 pm',
-  '1 pm',
-  '2 pm',
-  '3 pm',
-  '4 pm',
-  '5 pm',
+  moment('9:00:00', 'h:mm:ss'),
+  moment('10:00:00', 'h:mm:ss'),
+  moment('11:00:00', 'h:mm:ss'),
+  moment('12:00:00', 'h:mm:ss'),
+  moment('13:00:00', 'h:mm:ss'),
+  moment('14:00:00', 'h:mm:ss'),
+  moment('15:00:00', 'h:mm:ss'),
+  moment('16:00:00', 'h:mm:ss'),
+  moment('17:00:00', 'h:mm:ss'),
 ];
 
 
@@ -48,7 +48,7 @@ function renderHours() {
     inputPrepend.addClass('input-group-prepend').attr('id', 'input-group-prepend-' + i);
 
     var inputLabel = $('<label>');
-    inputLabel.addClass('input-group-text hour').text(hours[i]);
+    inputLabel.addClass('input-group-text hour').text(hours[i].format('hh a'));
 
     var inputFill = $('<textarea>');
     inputFill.addClass('form-control').attr('id', 'input-' + i).attr('data-color', 'future').attr('name', 'taskInput');
@@ -59,6 +59,8 @@ function renderHours() {
 
     var inputSubmit = $('<button>');
     inputSubmit.addClass('btn btn-primary saveBtn').attr('type', 'submit').attr('id', 'input-submit-' + i).text('save');
+
+    var blockTime = moment(hours[i]).format('hh a');
 
     inputPrepend.append(inputLabel);
     inputAppend.append(inputSubmit);
@@ -74,105 +76,145 @@ function renderHours() {
     rootEl.append(hourRow);
 
     function renderColors() {
-      if (currentHour == hours[i]) {
-        inputFill.attr('data-color', 'present');
+        if (moment().isSame( hours[i])) {
+          inputFill.attr('data-color', 'present');
 
+          // } else if (currentHour hours[i]) {
+          //   inputFill.attr('data-color', 'future');
+        } else if (moment().isAfter(hours[i])) {
+          inputFill.attr('data-color', 'past');
+        } else {
+          inputFill.attr('data-color', 'future');
+        }
       }
+    
+
+
+      // function submitTask(event) {
+      //   event.preventDefault();
+      //   var thisTask = inputFill.val();
+      //   localStorage.setItem('task' , thisTask);
+
+      // }
+
+      function submitTask0(event) {
+        event.preventDefault();
+        var input0 = $('#input-0').val();
+        localStorage.setItem('9 am', input0);
+      }
+
+      function submitTask1(event) {
+        event.preventDefault();
+        var input1 = $('#input-1').val();
+        localStorage.setItem('10 am', input1);
+      }
+
+      function submitTask2(event) {
+        event.preventDefault();
+        var input2 = $('#input-2').val();
+        localStorage.setItem('11 am', input2);
+      }
+
+      function submitTask3(event) {
+        event.preventDefault();
+        var input3 = $('#input-3').val();
+        localStorage.setItem('12 pm', input3);
+      }
+
+      function submitTask4(event) {
+        event.preventDefault();
+        var input4 = $('#input-4').val();
+        localStorage.setItem('1 pm', input4);
+      }
+
+      function submitTask5(event) {
+        event.preventDefault();
+        var input5 = $('#input-5').val();
+        localStorage.setItem('2 pm', input5);
+      }
+
+      function submitTask6(event) {
+        event.preventDefault();
+        var input6 = $('#input-6').val();
+        localStorage.setItem('3 pm', input6);
+      }
+
+      function submitTask7(event) {
+        event.preventDefault();
+        var input7 = $('#input-7').val();
+        localStorage.setItem('4 pm', input7);
+      }
+
+      function submitTask8(event) {
+        event.preventDefault();
+        var input8 = $('#input-8').val();
+        localStorage.setItem('5 pm', input8);
+      }
+
+      function renderTasks() {
+        if (localStorage.getItem('9 am')) {
+          $('#input-0').text(localStorage.getItem('9 am'));
+        }
+        if (localStorage.getItem('10 am')) {
+          $('#input-1').text(localStorage.getItem('10 am'));
+        }
+        if (localStorage.getItem('11 am')) {
+          $('#input-2').text(localStorage.getItem('11 am'));
+        }
+        if (localStorage.getItem('12 pm')) {
+          $('#input-3').text(localStorage.getItem('12 pm'));
+        }
+        if (localStorage.getItem('1 pm')) {
+          $('#input-4').text(localStorage.getItem('1 pm'));
+        }
+        if (localStorage.getItem('2 pm')) {
+          $('#input-5').text(localStorage.getItem('2 pm'));
+        }
+        if (localStorage.getItem('3 pm')) {
+          $('#input-6').text(localStorage.getItem('3 pm'));
+        }
+        if (localStorage.getItem('4 pm')) {
+          $('#input-7').text(localStorage.getItem('4 pm'));
+        }
+        if (localStorage.getItem('5 pm')) {
+          $('#input-8').text(localStorage.getItem('5 pm'));
+        }
+      }
+
+
+
+      $('#form-0').on('submit', submitTask0);
+      $('#form-1').on('submit', submitTask1);
+      $('#form-2').on('submit', submitTask2);
+      $('#form-3').on('submit', submitTask3);
+      $('#form-4').on('submit', submitTask4);
+      $('#form-5').on('submit', submitTask5);
+      $('#form-6').on('submit', submitTask6);
+      $('#form-7').on('submit', submitTask7);
+      $('#form-8').on('submit', submitTask8);
+
+      renderColors();
+
+      renderTasks();
+
+      // function renderColors() {
+      //   if (currentHour.isBefore(hours[i])) {
+      //     inputFill.data('color', 'future');
+      //   } else if (currentHour.isAfter(hours[i])) {
+      //     inputFill.data('color', 'past');
+      //   } else {
+      //     inputFill.data.('color', 'present');
+      //   }
+      // }
+
     }
-
-    // function submitTask(event) {
-    //   event.preventDefault();
-    //   var thisTask = inputFill.val();
-    //   localStorage.setItem('task' , thisTask);
-
-    // }
-
-    function submitTask0(event) {
-      event.preventDefault();
-      var input0 = $('#input-0').val();
-      localStorage.setItem('9 am', input0);
-    }
-
-    function submitTask1(event) {
-      event.preventDefault();
-      var input1 = $('#input-1').val();
-      localStorage.setItem('10 am', input1);
-    }
-
-    function submitTask2(event) {
-      event.preventDefault();
-      var input2 = $('#input-2').val();
-      localStorage.setItem('11 am', input2);
-    }
-
-    function submitTask3(event) {
-      event.preventDefault();
-      var input3 = $('#input-3').val();
-      localStorage.setItem('12 pm', input3);
-    }
-
-    function submitTask4(event) {
-      event.preventDefault();
-      var input4 = $('#input-4').val();
-      localStorage.setItem('1 pm', input4);
-    }
-
-    function submitTask5(event) {
-      event.preventDefault();
-      var input5 = $('#input-5').val();
-      localStorage.setItem('2 pm', input5);
-    }
-
-    function submitTask6(event) {
-      event.preventDefault();
-      var input6 = $('#input-6').val();
-      localStorage.setItem('3 pm', input6);
-    }
-
-    function submitTask7(event) {
-      event.preventDefault();
-      var input7 = $('#input-7').val();
-      localStorage.setItem('4 pm', input7);
-    }
-
-    function submitTask8(event) {
-      event.preventDefault();
-      var input8 = $('#input-8').val();
-      localStorage.setItem('5 pm', input8);
-    }
-
-
-
-    $('#form-0').on('submit', submitTask0);
-    $('#form-1').on('submit', submitTask1);
-    $('#form-2').on('submit', submitTask2);
-    $('#form-3').on('submit', submitTask3);
-    $('#form-4').on('submit', submitTask4);
-    $('#form-5').on('submit', submitTask5);
-    $('#form-6').on('submit', submitTask6);
-    $('#form-7').on('submit', submitTask7);
-    $('#form-8').on('submit', submitTask8);
-
-    renderColors();
-
-
-
-    // function renderColors() {
-    //   if (currentHour.isBefore(hours[i])) {
-    //     inputFill.data('color', 'future');
-    //   } else if (currentHour.isAfter(hours[i])) {
-    //     inputFill.data('color', 'past');
-    //   } else {
-    //     inputFill.data.('color', 'present');
-    //   }
-    // }
-
   }
-}
 
-//for each i append a form with label and button. label text = array[i]
-//label to be assigned class .hour and id  #i hour
-//button to be assigned class .saveBtn
-//
+  //for each i append a form with label and button. label text = array[i]
+  //label to be assigned class .hour and id  #i hour
+  //button to be assigned class .saveBtn
+  //
 
-renderHours();
+  renderHours();
+  console.log(currentHour);
+  
