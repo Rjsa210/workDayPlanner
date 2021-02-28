a44be9528730f17645380273056bea08ff9b09c3
Reid Johnson <rjsa210@gmail.com>
2021-02-28 10:51:46 -0800

    Javascript refactored and commented out. full functionality -RJ

--- Develop/script3.js
+++
@@ -1,8 +0,0 @@
-var rootEl = $('#root');
-
-var dateTimeNow = moment().format('dddd, MMMM Do YYYY, h:mm a');
-var currentHour = moment().format('h a')
-$('#currentDay').text(dateTimeNow);
-
-//create block for each hour
-  //create eventlistender for eache block
